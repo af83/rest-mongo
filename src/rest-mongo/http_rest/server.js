eb359b9a592b6c21e3b62ad32123ae6474e61fc2
Virtuo <pierre@ruyssen.fr>
2010-12-02 15:00:29 +0100

    Refacto on http_rest server.

--- src/rest-mongo/http_rest/server.js
+++ src/rest-mongo/http_rest/server.js
@@ -58,6 +58,121 @@ var get_body_json = exports.get_body_json = function(request, callback) {
   });
 };
 
+// --------------------------------------------------------------
+
+var Server = function(RFactory, class_name, schema) {
+  /* Class to hold context info about a particular resource.
+   *
+   * Arguments:
+   *  - RFactory: R factory function to user at each request.
+   *  - class_name: the class name of the resource to serve.
+   *  - schema: the schema corresponding to the resource.
+   *
+   */
+  var self = this;
+  this.RFactory = RFactory;
+  this.class_name = class_name;
+  this.schema = schema;
+  var proto = Server.prototype;
+  // Here we make the functions "bindable":
+  // we can call them directly, will be called on the this (Server) object.
+  for(var fct_name in proto) (function(fct_name, fct){
+    self[fct_name] = function() {
+      fct.apply(self, arguments);
+    };
+  })(fct_name, proto[fct_name]);
+};
+Server.prototype = {};
+
+Server.prototype.GET = function(response, _, data) {
+  /* GET on /resource where resource correspond to class_name.
+   */
+  var R = this.RFactory();
+  var u_query = {}; 
+  if (data && data.query != undefined) try {
+    u_query = JSON.parse(data.query);
+  } catch(e) {
+    response.writeHead(400);
+    response.end();
+  }
+  var query = {};
+  debug(u_query);
+  for(var criteria in u_query) { 
+    if (criteria in this.schema.properties || criteria in index_options) {
+      query[criteria] = u_query[criteria];
+    }
+  }
+  R[this.class_name].index({query: query}, function(objects) {
+    send_objects(objects, response);
+  });
+};
+
+Server.prototype.POST = function(response, _, data) {
+  // TODO: check data more carefully (handle mandatory stuff)
+  var R = this.RFactory();
+  obj = new R[this.class_name](data);
+  obj.save(function() {
+    response.writeHead(201);
+    response.write(JSON.stringify(obj.json()));
+    response.end();
+  });
+};
+
+Server.prototype.DELETE_ = function(response) {
+  var R = this.RFactory();
+  R[this.class_name].clear_all(function() {
+     response.writeHead(200);
+     response.end();
+  }, function(error) {
+    response.writeHead(500);
+    response.end();
+  });
+};
+
+Server.prototype.GETs = function(response, match) {
+  var ids = match[1].split(",");
+  var R = this.RFactory();
+  R[this.class_name].get({ids: ids}, function(objects) {
+    if(ids.length == 1) {
+      if(objects) return send_object(objects, response);
+      response.writeHead(404);
+      response.end();
+    }
+    else {
+      // XXX: handle 404 if missing one?
+      objects = objects.filter(function(obj) {
+        return obj != null;  
+      });
+      send_objects(objects, response);
+    }
+  });
+};
+
+Server.prototype.PUT = function(response, match, data) {
+  var ids = match[1].split(",");
+  var R = this.RFactory();
+  R[this.class_name].update({ids: ids, data: data}, function(objects) {
+    response.writeHead(200);
+    response.end();
+  }, function(error) {
+    response.writeHead(500);
+    response.end();
+  });
+};
+
+Server.prototype.DELETEs = function(response, match) {
+  var ids = match[1].split(",");
+  var R = this.RFactory();
+  R[this.class_name].delete_({ids: ids}, function() {
+    response.writeHead(200);
+    response.end();
+  }, function(err) {
+    response.writeHead(500);
+    response.end();
+  });
+};
+
+// --------------------------------------------------------------
 
 exports.connector = function(RFactory, schema, auth_check) {
   /** Returns a connect composant answering REST API calls.
@@ -89,114 +204,25 @@ exports.connector = function(RFactory, schema, auth_check) {
 
   utils.each(schema, function(class_name, data) {
     var resource = data.resource;
+    var escaped_resource = RegExp.escape(resource);
     var schema = data.schema;
     var route;
 
-    // GET or POST to /resource
-    routing.push([new RegExp('^' + resource + '$'), {
+    var server = new Server(RFactory, class_name, schema);
 
-      // Index
-      'GET': function(response, _, data) {
-        var R = RFactory();
-        var u_query = {}; 
-        if (data && data.query != undefined) try {
-          u_query = JSON.parse(data.query);
-        } catch(e) {
-          response.writeHead(400);
-          response.end();
-        }
-        var query = {};
-        //debug(schema);
-        debug(u_query);
-        for(var criteria in u_query) { 
-          if (criteria in schema.properties || criteria in index_options) {
-            query[criteria] = u_query[criteria];
-          }
-        }
-        R[class_name].index({query: query}, function(objects) {
-          send_objects(objects, response);
-        });
-      },
-
-      // Create
-      'POST': function(response, _, data) {
-        // TODO: check data more carefully (handle mandatory stuff)
-        var R = RFactory();
-        obj = new R[class_name](data);
-        obj.save(function() {
-          response.writeHead(201);
-          response.write(JSON.stringify(obj.json()));
-          response.end();
-        });
-      },
-
-      // Delete whole collection
-      'DELETE': function(response) {
-        var R = RFactory();
-        R[class_name].clear_all(function() {
-           response.writeHead(200);
-           response.end();
-        }, function(error) {
-          response.writeHead(500);
-          response.end();
-        });
-      }
-
+    // GET or POST to /resource (collection)
+    routing.push([new RegExp('^' + resource + '$'), 
+    { 'GET': server.GET // index
+    , 'POST': server.POST // Create
+    , 'DELETE': server.DELETE_ // Delete whole collection
     }]);
-
 
     // /resource/id1[,id2[,...]]
-    routing.push([new RegExp('^' + resource + "/(\\w+(?:,\\w+)*)$"), {
-
-      // GETs /resource/id1[,id2[,...]]
-      'GET': function(response, match) {
-        var ids = match[1].split(",");
-        var R = RFactory();
-        R[class_name].get({ids: ids}, function(objects) {
-          if(ids.length == 1) {
-            if(objects) return send_object(objects, response);
-            response.writeHead(404);
-            response.end();
-          }
-          else {
-            // XXX: handle 404 if missing one?
-            objects = objects.filter(function(obj) {
-              return obj != null;  
-            });
-            send_objects(objects, response);
-          }
-        });
-      },
-      
-      // Update
-      'PUT': function(response, match, data) {
-        var ids = match[1].split(",");
-        var R = RFactory();
-        R[class_name].update({ids: ids, data: data}, function(objects) {
-          response.writeHead(200);
-          response.end();
-        }, function(error) {
-          response.writeHead(500);
-          response.end();
-        });
-      },
-
-      // DELETE /resource/id1,id2[...]
-      'DELETE': function(response, match) {
-        var ids = match[1].split(",");
-        var R = RFactory();
-        R[class_name].delete_({ids: ids}, function() {
-          response.writeHead(200);
-          response.end();
-        }, function(err) {
-          response.writeHead(500);
-          response.end();
-        });
-      }
-
+    routing.push([new RegExp('^' + resource + "/(\\w+(?:,\\w+)*)$"),
+    { 'GET': server.GETs // GETs /resource/id1[,id2[,...]]
+    , 'PUT': server.PUT // Update
+    , 'DELETE': server.DELETEs // DELETE /resource/id1,id2[...] 
     }]);
-
-
   });
 
   return function(request, response, next) {
